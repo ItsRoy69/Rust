1befcb4553214377a7ded003d9b6b94e428eb793
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-12-25 00:30:38 +0530

    Initial installation

---
+++ Learn/two/target/debug/deps/two.d
@@ -0,0 +1,5 @@
+E:\Github\Rust\Learn\two\target\debug\deps\two.exe: src\main.rs
+
+E:\Github\Rust\Learn\two\target\debug\deps\two.d: src\main.rs
+
+src\main.rs:
---
+++ Learn/two/target/debug/two.d
@@ -0,0 +1,1 @@
+E:\Github\Rust\Learn\two\target\debug\two.exe: E:\Github\Rust\Learn\two\src\main.rs
