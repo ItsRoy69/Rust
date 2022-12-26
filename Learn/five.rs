c87c8dfed5979c6e74b8fb97c48c3c83a9f028e3
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-12-27 01:42:49 +0530

    Create five.rs

---
+++ Learn/five.rs
@@ -0,0 +1,8 @@
+// arrays
+
+fn main() {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    println!("First month = {}", months[0]);
+    
+}
